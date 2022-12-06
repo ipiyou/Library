90602716fe5fcf37ee70316cd41838e213683b04
ipiyou <tao710803@gmail.com>
2022-12-06 20:07:12 +0900

    route 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import MainRoot from "./pages/MainRoot";
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<MainRoot />} />
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Suspense } from "react";
+import App from "./App";
+import "./body.css"
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Suspense>
+      <App />
+    </Suspense>
+  </BrowserRouter>
 );
