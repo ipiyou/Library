e8f50408aa2d2e862adf4af515c9e6825b4a6500
ipiyou <tao710803@gmail.com>
2022-12-14 18:51:33 +0900

    각도에 따른 원의 좌표

--- src/components/function/mussen/pickcolor/index.tsx
+++ src/components/function/mussen/pickcolor/index.tsx
@@ -1,8 +1,34 @@
-import { useRef, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import "./style.css";
+
+interface PosType {
+  curX: number;
+  curY: number;
+}
+
+const getAbsoluteLocation = (angle: number) => {
+  const x = Math.cos((angle * Math.PI) / 180) * 100;
+  const y = Math.sin((angle * Math.PI) / 180) * 100;
+};
+
+const quadrantSelector = (width: number, height: number, pos: PosType) => {
+  const { curX, curY } = pos;
+  if (curX > width / 2) {
+
+  } else {
+  }
+  if (curY) {
+  } else {
+  }
+  const angle = Math.atan(curX / curY) / (Math.PI / 180);
+};
 
 function ColorPicker() {
   const Ref = useRef<HTMLCanvasElement>(null);
+  const [pos, setPos] = useState<PosType>({
+    curX: 0,
+    curY: 0,
+  });
 
   useEffect(() => {
     const canv = Ref.current;
@@ -10,12 +36,11 @@ function ColorPicker() {
       const { offsetX, clientY } = e;
       console.log(offsetX, clientY);
     });
-    
   }, []);
+
   return (
     <div className="colorpicker-wrap">
       <canvas id="colorpicker-canvas" ref={Ref} />
-      {Math.atan(5 / 100) / (Math.PI / 180)}
     </div>
   );
 }
